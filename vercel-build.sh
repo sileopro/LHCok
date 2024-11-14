d2f19a59a5f08b60df245a9852b753e9ffb98aaf
sileopro <hackersxt@outlook.com>
2024-11-14 20:40:55 +0800

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-# 安装 Chrome 和 ChromeDriver
+
+# 安装系统依赖
 apt-get update
-apt-get install -y google-chrome-stable chromium-chromedriver
+apt-get install -y wget gnupg2 unzip
+
+# 安装 Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+# 安装 ChromeDriver
+CHROME_VERSION=$(google-chrome --version | cut -d " " -f3 | cut -d "." -f1)
+wget -N https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION}
+CHROMEDRIVER_VERSION=$(cat LATEST_RELEASE_${CHROME_VERSION})
+wget https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
 
 # 安装 Python 依赖
-pip install -r requirements.txt 
+pip install -r requirements.txt
