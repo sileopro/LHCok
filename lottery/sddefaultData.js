39bf50ae054eb441e9211c1cd079f3db0ef0279a
sileopro <hackersxt@outlook.com>
2026-01-25 13:49:11 +0800

    Add files via upload

---
+++ lottery/sddefaultData.js
@@ -0,0 +1,24 @@
+// 3D开奖数据
+const sddefaultData = {
+  "2026": [
+    { period: "001", numbers: [2, 9, 8] },
+    { period: "002", numbers: [5, 2, 0] },
+    { period: "003", numbers: [6, 0, 1] },
+    { period: "004", numbers: [0, 1, 9] },
+    { period: "005", numbers: [4, 7, 6] },
+    { period: "006", numbers: [2, 4, 4] },
+    { period: "007", numbers: [3, 5, 3] },
+    { period: "008", numbers: [2, 5, 2] },
+    { period: "009", numbers: [2, 6, 5] },
+    { period: "010", numbers: [6, 6, 7] },
+    { period: "011", numbers: [6, 4, 7] },
+    { period: "012", numbers: [2, 4, 1] },
+    { period: "013", numbers: [5, 1, 3] },
+    { period: "014", numbers: [0, 5, 0] },
+    { period: "015", numbers: [5, 3, 2] },
+    { period: "016", numbers: [5, 8, 2] },
+    { period: "017", numbers: [9, 4, 5] },
+    { period: "018", numbers: [4, 9, 4] },
+    { period: "019", numbers: [2, 2, 3] }
+  ]
+};
