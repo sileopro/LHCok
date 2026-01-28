abdb5896d45c11bc8fe0047790cadd1f7789a763
sileopro <hackersxt@outlook.com>
2026-01-28 10:51:16 +0800

    Update ssqdefaultData.js

--- lottery/ssqdefaultData.js
+++ lottery/ssqdefaultData.js
@@ -1,6 +1,6 @@
 // 双色球开奖数据
 const ssqdefaultData = {
-  "2026": [
-    { period: "012", numbers: [3, 5, 7, 16, 20, 24, 8] }
+  "2025": [
+    { period: "312", numbers: [3, 5, 7, 16, 20, 24, 12] }
   ]
 };
