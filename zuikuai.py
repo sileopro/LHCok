9e950ce79c17b7314253030de70a46b71311d410
sileopro <hackersxt@outlook.com>
2026-03-07 00:05:13 +0800

    Update zuikuai.py

--- zuikuai.py
+++ zuikuai.py
@@ -236,6 +236,8 @@ def update_hkrc_file(issue_str, special_number, special_zodiac):
         with open(filename, 'w', encoding='utf-8') as f:
             for k in sorted(issue_to_line.keys(), reverse=True):
                 f.write(issue_to_line[k] + '\n')
+        # 日志中先打印具体内容，再打印保存成功提示
+        logger.info(line)
         logger.info("✅ 已保存港彩日冲记录到 hkrc.txt")
     except Exception as e:
         logger.error(f"更新港彩日冲记录失败: {str(e)}")
