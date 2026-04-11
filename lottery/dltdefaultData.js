53b4dd946f4d0856902fb1850128d295f9bb0804
sileopro <hackersxt@outlook.com>
2026-04-11 11:53:44 +0800

    Update dltdefaultData.js

--- lottery/dltdefaultData.js
+++ lottery/dltdefaultData.js
@@ -1,1 +1,12 @@
-sddefaultData.js
+// 大乐透开奖数据
+const ssqdefaultData = {
+  "2004": [
+    { period: "001", numbers: [1, 2, 7, 10, 25, 3,7] },
+    { period: "002", numbers: [3, 11, 14, 24, 31, 5,11] }
+  ],
+  "2026": [
+    { period: "001", numbers: [1, 2, 7, 10, 25, 3,12] },
+    { period: "002", numbers: [3, 11, 14, 24, 31, 2,8] }
+   
+  ]
+};
